9c5ad88929027ba470946d9dea12be53157cc38b
Mikhitatyan Evgeny <76537596+Evg999@users.noreply.github.com>
2023-04-27 18:30:09 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+// данные для подключения к БД 
+    const DB_HOST = 'localhost';
+    const DB_USER = 'zzzvas3d_mag24';
+    const DB_PASS = '3ZqQDaAr';
+    const DB_NAME = 'zzzvas3d_mag24';
