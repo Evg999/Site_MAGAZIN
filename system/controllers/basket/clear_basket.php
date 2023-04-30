ba15885b0f2f7a3b45dd8795d4143360b5767879
Mikhitatyan Evgeny <76537596+Evg999@users.noreply.github.com>
2023-05-01 00:28:54 +0300

    Delete system directory

--- system/controllers/basket/clear_basket.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-// Полная очистка корзины через кнопку
-
-$_SESSION['basket'] = [];
-
-header('Location:'.$_SERVER['HTTPS_REFERER']);
--- system/controllers/users/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-// для кнопки Выход
-
-setcookie('user_id',0,time() - 3600,'/');// убиваем куки
-header('Location: https://mag24-shop.ru/catalog.php');// редерект в каталог
-?>
